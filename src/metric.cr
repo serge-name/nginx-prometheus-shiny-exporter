9fee3fe3b2ff059bb55d0819dc9b345c005e9d63
Serge Pokhodyaev <snp@z9.ru>
2018-07-30 16:31:07 +0300

    refactor Metric* classes

--- src/metric.cr
+++ src/metric.cr
@@ -1,17 +1,40 @@
+module Metric(T)
+
+  def initialize
+    @val = Hash(String, Hash(String, T)).new
+  end
+
+end
+
+
 class MetricStatusCounter
 
   METRIC_NAME = "nginx_request_status"
 
-  def initialize
-    @val = Hash(String, Hash(String, Hash(UInt16, UInt64))).new
+  class Item
+    def initialize
+      @val = Hash(UInt16, UInt64).new
+    end
+
+    def each
+      @val.each_key do |k|
+        yield({k, @val[k]})
+      end
+    end
+
+    def put(status)
+      @val[status] = 0 unless @val.has_key?(status)
+
+      @val[status] += 1_u64
+    end
   end
 
-  def inc(host, tag, status)
-    @val[host] = Hash(String, Hash(UInt16, UInt64)).new unless @val.has_key?(host)
-    @val[host][tag] = Hash(UInt16, UInt64).new unless @val[host].has_key?(tag)
-    @val[host][tag][status] = 0 unless @val[host][tag].has_key?(status)
+  include Metric(Item)
 
-    @val[host][tag][status] += 1_u64
+  def inc(host, tag, status)
+    @val[host] = Hash(String, Item).new unless @val.has_key?(host)
+    @val[host][tag] = Item.new unless @val[host].has_key?(tag)
+    @val[host][tag].put(status)
   end
 
   def to_s(io)
@@ -33,12 +56,12 @@ class MetricStatusCounter
         m_499     = 0_u64
         m_500_599 = 0_u64
 
-        @val[h][t].each_key do |s|
-          m += "#{METRIC_NAME}{host=\"#{h}\",tag=\"#{t}\",status=\"#{s}\"} #{@val[h][t][s]}\n"
-          m_100_399 += @val[h][t][s] if s >= 100 && s <= 399
-          m_400_498 += @val[h][t][s] if s >= 400 && s <= 498
-          m_499 += @val[h][t][s]     if s == 499
-          m_500_599 += @val[h][t][s] if s >= 500 && s <= 599
+        @val[h][t].each do |s, v|
+          m += "#{METRIC_NAME}{host=\"#{h}\",tag=\"#{t}\",status=\"#{s}\"} #{v}\n"
+          m_100_399 += v if s >= 100 && s <= 399
+          m_400_498 += v if s >= 400 && s <= 498
+          m_499     += v     if s == 499
+          m_500_599 += v if s >= 500 && s <= 599
         end
 
         m += "#{METRIC_NAME}_100_399{host=\"#{h}\",tag=\"#{t}\"} #{m_100_399}\n" if m_100_399 > 0
@@ -53,28 +76,61 @@ class MetricStatusCounter
 
 end
 
+
 class MetricReqTime
 
   METRIC_NAME = "nginx_request_time"
-  TIME_STEP = Time::Span.new(seconds: 10, nanoseconds: 0) # FIXME: make it configurable
 
-  def initialize
-    @val = Hash(String, Hash(String, Hash(Symbol, Float64 | UInt64 | Time))).new
-  end
+  class Item
 
-  def register(host, tag, time)
-    @val[host] = Hash(String, Hash(Symbol, Float64 | UInt64 | Time)).new unless @val.has_key?(host)
-    @val[host][tag] = Hash(Symbol, Float64 | UInt64 | Time).new unless @val[host].has_key?(tag)
-    unless @val[host][tag].has_key?(:timestamp) && (Time.utc_now - @val[host][tag][:timestamp].as(Time)) <= TIME_STEP
-      @val[host][tag][:c_time] = 0_f64
-      @val[host][tag][:c_req] = 0_u64
-      @val[host][tag][:time_max] = 0_f64
-      @val[host][tag][:timestamp] = Time.utc_now
+    TIME_STEP = Time::Span.new(seconds: 10, nanoseconds: 0) # FIXME: make it configurable
+
+    def initialize
+      # FIXME: rm redundancy
+      @c_time    = 0_f64
+      @c_req     = 0_u64
+      @time_max  = 0_f64
+      @timestamp = Time.utc_now
     end
 
-    @val[host][tag][:c_time] = @val[host][tag][:c_time].as(Float64) + time
-    @val[host][tag][:time_max] = time if @val[host][tag][:time_max].as(Float64) < time
-    @val[host][tag][:c_req] = @val[host][tag][:c_req].as(UInt64) + 1_u64
+    def reset
+      @c_time    = 0_f64
+      @c_req     = 0_u64
+      @time_max  = 0_f64
+      @timestamp = Time.utc_now
+    end
+
+    def fresh?
+      (Time.utc_now - @timestamp.as(Time)) <= TIME_STEP
+    end
+
+    def counted?
+      @c_req > 0
+    end
+
+    def avg
+      @c_time / @c_req
+    end
+
+    def max
+      @time_max
+    end
+
+    def put(time)
+      reset unless fresh?
+
+      @c_time   += time
+      @time_max  = time if @time_max < time
+      @c_req    += 1_u64
+    end
+  end
+
+  include Metric(Item)
+
+  def register(host, tag, time)
+    @val[host] = Hash(String, Item).new unless @val.has_key?(host)
+    @val[host][tag] = Item.new unless @val[host].has_key?(tag)
+    @val[host][tag].put(time)
   end
 
   def to_s(io)
@@ -91,10 +147,9 @@ class MetricReqTime
 
     @val.each_key do |h|
       @val[h].each_key do |t|
-        if (@val[h][t][:c_req].as(UInt64) > 0) && (Time.utc_now - @val[h][t][:timestamp].as(Time)) <= TIME_STEP
-          avg = @val[h][t][:c_time].as(Float64) / @val[h][t][:c_req].as(UInt64)
-          m += "#{METRIC_NAME}_avg{host=\"#{h}\",tag=\"#{t}\"\"} #{avg}\n"
-          m += "#{METRIC_NAME}_max{host=\"#{h}\",tag=\"#{t}\"\"} #{@val[h][t][:time_max]}\n"
+        if @val[h][t].counted? && @val[h][t].fresh?
+          m += "#{METRIC_NAME}_avg{host=\"#{h}\",tag=\"#{t}\"\"} #{@val[h][t].avg}\n"
+          m += "#{METRIC_NAME}_max{host=\"#{h}\",tag=\"#{t}\"\"} #{@val[h][t].max}\n"
         end
       end
     end
